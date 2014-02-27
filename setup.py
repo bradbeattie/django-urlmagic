b982c63b37af5503e6a0438752e0d326ae9f2bd4
Brad Beattie <brad@gossamer-threads.com>
2014-02-26 16:46:42 -0800

    Distribution

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='django-urlmagic',
+    version='0.1',
+    packages=['urlmagic',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+    url='https://github.com/bradbeattie/django-urlmagic',
+    author='Brad Beattie',
+    author_email='bradbeattie@gmail com',
+)
