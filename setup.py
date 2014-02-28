72bd5999a165bb4fcbec1fe861df8ac89e196b3f
Brad Beattie <brad@gossamer-threads.com>
2014-02-27 16:15:22 -0800

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-urlmagic',
-    version='0.3.2',
+    version='0.3.3',
     packages=['urlmagic',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
